349d442ed7def4e6f0a23ec2033a9963ae6a94db
eskilwennevold <eskilwennevold@hotmail.com>
2021-05-17 13:11:21 +0200

    Første Upload

---
+++ src/main/java/com/nordicmotorhomes/NordicmotorhomesApplication.java
@@ -0,0 +1,13 @@
+package com.nordicmotorhomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicmotorhomesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordicmotorhomesApplication.class, args);
+    }
+
+}
