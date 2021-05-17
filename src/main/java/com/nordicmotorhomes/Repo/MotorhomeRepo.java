7b34ab5ee993c23c3cd74e921176b08f3075272c
eskilwennevold <eskilwennevold@hotmail.com>
2021-05-17 15:31:12 +0200

    Eskil Grundlag Endeligt 2

--- src/main/java/com/nordicmotorhomes/Repo/MotorhomeRepo.java
+++ src/main/java/com/nordicmotorhomes/Repo/MotorhomeRepo.java
@@ -1,7 +1,11 @@
 package com.nordicmotorhomes.Repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MotorhomeRepo {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 }
--- src/main/java/com/nordicmotorhomes/Repo/RentalRepo.java
+++ src/main/java/com/nordicmotorhomes/Repo/RentalRepo.java
@@ -1,7 +1,11 @@
 package com.nordicmotorhomes.Repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RentalRepo {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 }
