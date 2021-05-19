b5c95cc745aa3d77aaafe8415e72ea85028ea6d4
thorminathor4 <thorfa4444@gmail.com>
2021-05-19 13:35:04 +0200

    Thor Controller menu metoder og HTML Menuer sider

--- src/main/java/com/nordicmotorhomes/Controller/HomeController.java
+++ src/main/java/com/nordicmotorhomes/Controller/HomeController.java
@@ -5,6 +5,7 @@ import com.nordicmotorhomes.Service.MotorhomeService;
 import com.nordicmotorhomes.Service.RentalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController{
@@ -14,6 +15,20 @@ public class HomeController{
     MotorhomeService motorhomeService;
     @Autowired
     RentalService rentalService;
-
-
+    @GetMapping("/")
+    public String mainMenu(){
+        return "/mainMenu";
+    }
+    @GetMapping("/motorhomeMenu")
+    public String motorhomeMenu(){
+        return "/motorhomeMenu";
+    }
+    @GetMapping("/customerMenu")
+    public String customerMenu(){
+        return "/customerMenu";
+    }
+    @GetMapping("/rentalMenu")
+    public String rentalMenu(){
+        return "/rentalMenu";
+    }
 }
