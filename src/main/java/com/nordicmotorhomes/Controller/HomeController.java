5e643db8aacd9ac969ac31f0232237c265283c1e
thorminathor4 <thorfa4444@gmail.com>
2021-05-19 13:52:14 +0200

    Thor Controller Menu metoder med model attributes

--- src/main/java/com/nordicmotorhomes/Controller/HomeController.java
+++ src/main/java/com/nordicmotorhomes/Controller/HomeController.java
@@ -5,6 +5,7 @@ import com.nordicmotorhomes.Service.MotorhomeService;
 import com.nordicmotorhomes.Service.RentalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,20 +16,27 @@ public class HomeController{
     MotorhomeService motorhomeService;
     @Autowired
     RentalService rentalService;
+
     @GetMapping("/")
     public String mainMenu(){
         return "/mainMenu";
     }
+
     @GetMapping("/motorhomeMenu")
-    public String motorhomeMenu(){
+    public String motorhomeMenu(Model model){
+        model.addAttribute("motorhomes",motorhomeService.loadAllMotorhomes());
         return "/motorhomeMenu";
     }
+
     @GetMapping("/customerMenu")
-    public String customerMenu(){
+    public String customerMenu(Model model){
+        model.addAttribute("customers",customerService.loadAllCustomers());
         return "/customerMenu";
     }
+
     @GetMapping("/rentalMenu")
-    public String rentalMenu(){
+    public String rentalMenu(Model model){
+        model.addAttribute("rentals",rentalService.loadAllRentals());
         return "/rentalMenu";
     }
 }
